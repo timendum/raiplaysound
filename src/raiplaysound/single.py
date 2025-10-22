0eecf54c7783525c04ebb7cd820c49e8618654cd
timendum <timedum@gmail.com>
2025-10-22 15:51:05 +0200

    write only on change

--- src/raiplaysound/single.py
+++ src/raiplaysound/single.py
@@ -7,7 +7,8 @@ from os.path import join as pathjoin
 from urllib.parse import urljoin
 
 import requests
-from feedendum import Feed, FeedItem, to_rss_string
+from feedendum import Feed, FeedItem, from_rss_file, to_rss_string
+from feedendum.exceptions import FeedXMLError
 
 NSITUNES = "{http://www.itunes.com/dtds/podcast-1.0.dtd}"
 
@@ -59,7 +60,7 @@ class RaiParser:
     def _json_to_feed(self, feed: Feed, rdata) -> None:
         feed.title = rdata["title"]
         feed.description = rdata["podcast_info"].get("description", "")
-        feed.description = feed.description or rdata["title"]
+        feed.description = (feed.description or rdata["title"]).strip()
         feed.url = self.url
         feed._data["image"] = {"url": urljoin(self.url, rdata["podcast_info"]["image"])}
         feed._data[f"{NSITUNES}author"] = "RaiPlaySound"
@@ -101,7 +102,7 @@ class RaiParser:
             dupdate = _datetime_parser(item["create_date"] + " " + item["create_time"])
             fitem.update = dupdate
             fitem.url = urljoin(self.url, item["track_info"]["page_url"])
-            fitem.content = item.get("description", item["title"])
+            fitem.content = item.get("description", item["title"]).strip()
             fitem._data = {
                 "enclosure": {
                     "@type": "audio/mpeg",
@@ -187,12 +188,22 @@ class RaiParser:
         if feed.items:
             self._fix_dates(feed)
             filename = pathjoin(self.folderPath, url_to_filename(self.url))
-            atomic_write(filename, to_rss_string(feed))
-            print(f"Written {filename}")
+            if atomic_write(filename, feed):
+                print(f"Written {filename}")
+            else:
+                print(f"No changes for {filename}")
         return [feed] + self.inner
 
 
-def atomic_write(filename: str, content: str) -> None:
+def atomic_write(filename: str, content: Feed, always=True) -> bool:
+    if not always:
+        try:
+            with open(filename, encoding="utf8") as f:
+                old_feed = from_rss_file(f)
+                if compare_feed(old_feed, content):
+                    return False
+        except (OSError, FeedXMLError):
+            pass
     tmp = tempfile.NamedTemporaryFile(
         mode="w",
         encoding="utf8",
@@ -201,6 +212,43 @@ def atomic_write(filename: str, content: str) -> None:
         prefix=".tmp-single-",
         suffix=".xml",
     )
-    tmp.write(content)
+    tmp.write(to_rss_string(content))
     tmp.close()
     os.replace(tmp.name, filename)
+    return True
+
+
+def compare_feed(a: Feed, b: Feed) -> bool:
+    if a.title != b.title:
+        return False
+    if a.description != b.description:
+        return False
+    if a.url != b.url:
+        return False
+    if a._data.get("image", {}).get("url", None) != b._data.get("image", {}).get("url", None):
+        return False
+    # TODO? feed._data[f"{NSITUNES}category"]
+    # not update
+    if len(a.items) != len(b.items):
+        return False
+    for ia, ib in zip(a.items, b.items, strict=True):
+        if ia.title != ib.title:
+            return False
+        if ia.id != ib.id:
+            return False
+        # not update
+        if ia.url != ib.url:
+            return False
+        if ia.content != ib.content:
+            return False
+        if ia._data.get("enclosure", {}).get("@url", None) != ib._data.get("enclosure", {}).get(
+            "@url", None
+        ):
+            return False
+        if ia._data.get(f"{NSITUNES}duration", None) != ib._data.get(f"{NSITUNES}duration", None):
+            return False
+        if ia._data.get(f"{NSITUNES}episode", None) != ib._data.get(f"{NSITUNES}episode", None):
+            return False
+        if ia._data.get(f"{NSITUNES}season", None) != ib._data.get(f"{NSITUNES}season", None):
+            return False
+    return True
